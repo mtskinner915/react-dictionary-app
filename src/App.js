4951c67d189f761c6e80854d4afb3bf9ca70f491
mtskinner915 <margarettaylor915@gmail.com>
2022-07-13 13:08:21 -0700

    Setup react app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Margaret's Dictionary App Setup</h1>
+      </header>
+    </div>
+  );
+}
