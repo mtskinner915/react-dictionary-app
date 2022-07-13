c4f22ce017b48c2f4dea79048030cc8e3b1e89bd
mtskinner915 <margarettaylor915@gmail.com>
2022-07-13 14:58:32 -0700

    Created search engine and updated some CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Margaret's Dictionary App Setup</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Dictionary</h1>
+        </header>
+        <main>
+          <h2>What do you want to learn about?</h2>
+          <Dictionary />
+        </main>
+      </div>
+      <footer className="App-footer">Coded by Margaret Skinner</footer>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function Search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword} definition`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={Search}>
+        <input
+          type="search"
+          onChange={handleKeywordChange}
+          autoFocus={true}
+          placeholder="Type a word..."
+        />
+        <button>Search</button>
+      </form>
+    </div>
+  );
+}
