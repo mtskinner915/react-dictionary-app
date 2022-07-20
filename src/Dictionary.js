cbf42578b3d45dd0af8552f2a0823a57fe4b50aa
mtskinner915 <margarettaylor915@gmail.com>
2022-07-20 15:59:47 -0700

    Added photo section and adjusted CSS

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,22 +1,33 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
   function Search(event) {
     event.preventDefault();
 
     // https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f91700001000001cc5db9487b204d1f9f2b17d798d2a30e";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+    let header = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: header }).then(handlePexelsResponse);
   }
 
   function handleKeywordChange(event) {
@@ -36,6 +47,7 @@ export default function Dictionary() {
         <button className="search-button">Search</button>
       </form>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
